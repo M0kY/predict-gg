24bbe2f54ac18cd6c285906a4df4a58466ebe1ff
M0kY <46133205+M0kY@users.noreply.github.com>
2019-01-14 00:56:09 +0100

    Updated data filtering

--- cleanup.js
+++ cleanup.js
@@ -7,15 +7,29 @@ require('./src/dbconnect');
 const GameStatsModel = require('./src/models/gameStatsModel.js');
 const GameStatsCleanModel = require('./src/models/gameStatsCleanModel.js');
 
+getObjectKeys = (object) => {
+  return Object.keys(object).filter(key => 
+    key !== 'magicDamageDealtToChampions' && 
+    key !== 'physicalDamageDealtToChampions' && 
+    key !== 'magicDamageDealt' && 
+    key !== 'magicalDamageTaken' && 
+    key !== 'trueDamageTaken' && 
+    key !== 'trueDamageDealt' &&
+    key !== 'physicalDamageDealt' &&
+    key !== 'physicalDamageTaken' &&
+    key !== 'trueDamageDealtToChampions'
+  );
+}
+
 const main = async () => {
   const startTime = moment();
   try {
     const data = await GameStatsModel.find({})
     const cleanData = data.map(record => {
       const cleanStats = record.stats.map(playerStats => {
-        let gameStats = Array(70).fill(0);
+        let gameStats = Array(55).fill(0);
         playerStats.stats.map(game => {
-          const keys = Object.keys(game);
+          const keys = getObjectKeys(game);
           keys.map((key, index) => gameStats[index] += (typeof game[key] === 'boolean' ? +game[key] : game[key] /*/ game.gameDuration*/));
         })
         //gameStats = gameStats.map(stat => stat/playerStats.numberOfGames);
